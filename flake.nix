41986d56c64dd847ae8155178ce680774a23785d
TomekBobrowicz <129859305+TomekBobrowicz@users.noreply.github.com>
2026-08-17 17:19:40 +0200

    chore: use git URLs for GitHub flake inputs

--- flake.nix
+++ flake.nix
@@ -5,17 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
+      url = "git+https://github.com/NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "git+https://github.com/nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "git+https://github.com/noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -25,117 +25,113 @@
     };
 
     sops-nix = {
-      url = "github:Mic92/sops-nix";
+      url = "git+https://github.com/Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     codex-desktop = {
-      url = "github:ilysenko/codex-desktop-linux";
+      url = "git+https://github.com/ilysenko/codex-desktop-linux";
     };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      nixos-hardware,
-      home-manager,
-      noctalia,
-      kineticwe,
-      sops-nix,
-      codex-desktop,
-      ...
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nixos-hardware,
+    home-manager,
+    noctalia,
+    kineticwe,
+    sops-nix,
+    codex-desktop,
+    ...
+  }: let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+
+    mkHost = {
+      hostname,
+      hardwareProfile,
+      hardwareConfiguration,
+      hostModule,
     }:
-    let
-      system = "x86_64-linux";
-      lib = nixpkgs.lib;
+      lib.nixosSystem {
+        inherit system;
 
-      mkHost =
-        {
-          hostname,
-          hardwareProfile,
-          hardwareConfiguration,
-          hostModule,
-        }:
-        lib.nixosSystem {
-          inherit system;
+        specialArgs = {
+          inherit inputs hostname;
+        };
 
-          specialArgs = {
-            inherit inputs hostname;
-          };
+        modules = [
+          hardwareProfile
+          hardwareConfiguration
 
-          modules = [
-            hardwareProfile
-            hardwareConfiguration
+          ./modules/core
+          ./modules/core/boot.nix
+          ./modules/core/binary-compatibility.nix
+          ./modules/core/networking.nix
+          ./modules/core/nix.nix
+          ./modules/core/secrets.nix
+          ./modules/core/users.nix
 
-            ./modules/core
-            ./modules/core/boot.nix
-            ./modules/core/binary-compatibility.nix
-            ./modules/core/networking.nix
-            ./modules/core/nix.nix
-            ./modules/core/secrets.nix
-            ./modules/core/users.nix
+          ./modules/desktop/kde.nix
+          ./modules/desktop/audio.nix
+          ./modules/desktop/fonts.nix
+          ./modules/desktop/flatpak.nix
+          ./modules/desktop/kineticwe.nix
+          ./modules/desktop/noctalia.nix
 
-            ./modules/desktop/kde.nix
-            ./modules/desktop/audio.nix
-            ./modules/desktop/fonts.nix
-            ./modules/desktop/flatpak.nix
-            ./modules/desktop/kineticwe.nix
-            ./modules/desktop/noctalia.nix
+          ./modules/programs/common.nix
+          ./modules/printing
+          ./modules/services
+          ./modules/virtualization
 
-            ./modules/programs/common.nix
-            ./modules/printing
-            ./modules/services
-            ./modules/virtualization
+          hostModule
 
-            hostModule
+          home-manager.nixosModules.home-manager
+          sops-nix.nixosModules.sops
 
-            home-manager.nixosModules.home-manager
-            sops-nix.nixosModules.sops
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
 
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-
-                extraSpecialArgs = {
-                  inherit inputs hostname;
-                };
-
-                users.buber = import ./home;
-
-                sharedModules = [
-                  ./home/programs.nix
-                  ./home/starship.nix
-                  ./home/ghostty.nix
-                  ./home/git.nix
-                  ./home/kde.nix
-                  ./home/multimedia.nix
-                  ./home/noctalia.nix
-                  kineticwe.homeModules.default
-                ];
+              extraSpecialArgs = {
+                inherit inputs hostname;
               };
-            }
-          ];
-        };
-    in
-    {
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
 
-      nixosConfigurations = {
-        kinetix = mkHost {
-          hostname = "kinetix";
-          hardwareProfile = nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1;
-          hardwareConfiguration = ./hosts/kinetix/hardware-configuration.nix;
-          hostModule = ./hosts/kinetix/default.nix;
-        };
+              users.buber = import ./home;
 
-        fury = mkHost {
-          hostname = "fury";
-          hardwareProfile = nixos-hardware.nixosModules.gigabyte-b550;
-          hardwareConfiguration = ./hosts/fury/hardware-configuration.nix;
-          hostModule = ./hosts/fury/default.nix;
-        };
+              sharedModules = [
+                ./home/programs.nix
+                ./home/starship.nix
+                ./home/ghostty.nix
+                ./home/git.nix
+                ./home/kde.nix
+                ./home/multimedia.nix
+                ./home/noctalia.nix
+                kineticwe.homeModules.default
+              ];
+            };
+          }
+        ];
+      };
+  in {
+    formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+
+    nixosConfigurations = {
+      kinetix = mkHost {
+        hostname = "kinetix";
+        hardwareProfile = nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1;
+        hardwareConfiguration = ./hosts/kinetix/hardware-configuration.nix;
+        hostModule = ./hosts/kinetix/default.nix;
+      };
+
+      fury = mkHost {
+        hostname = "fury";
+        hardwareProfile = nixos-hardware.nixosModules.gigabyte-b550;
+        hardwareConfiguration = ./hosts/fury/hardware-configuration.nix;
+        hostModule = ./hosts/fury/default.nix;
       };
     };
+  };
 }
