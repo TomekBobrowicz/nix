5788aaf4b640b32d160c801f61a052f32b2941c0
TomekBobrowicz <129859305+TomekBobrowicz@users.noreply.github.com>
2026-08-17 16:41:57 +0200

    your change

--- home/git.nix
+++ home/git.nix
@@ -1,17 +1,18 @@
-{ ... }:
-{
+{...}: {
   programs.git = {
     enable = true;
+
     settings = {
-      user.name = "Tomek Bobrowicz";
+      user.name = "TomekBobrowicz";
       user.email = "129859305+TomekBobrowicz@users.noreply.github.com";
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = true;
-        push.autoSetupRemote = true;
-        fetch.prune = true;
-        core.editor = "nvim";
-      };
+
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      push.autoSetupRemote = true;
+      fetch.prune = true;
+      core.editor = "nvim";
+
+      credential.helper = "!gh auth git-credential";
     };
   };
 }
